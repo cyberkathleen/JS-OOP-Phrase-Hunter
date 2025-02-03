8492b5a19f9925e2d86574ca47d35ee000695894
Kathleen <67641959+cyberkathleen@users.noreply.github.com>
2025-02-03 13:06:23 +0100

    Add class Phrase

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,56 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  /**
+   * Adds letter placeholders to the display when the game starts.
+   * Each letter is presented by an empty box, onle <li> element for each letter.
+   */
+  addPhraseToDisplay() {
+    const phraseUl = document.querySelector('#phrase ul');
+    phraseUl.innerHTML = '';
+
+    this.phrase.forEach(char => {
+      const li = document.createElement('li');
+      
+      // Use the 'letter' or the 'space' CSS class depending on the character
+      if(char === ' ') {
+        li.classList.add('space');
+        li.textContent = ' ';
+      } else {
+        li.classList.add(`hide letter ${char}`);
+        li.textContent = char;
+      }
+
+      phraseUl.appendChild(li);
+    });
+  }
+
+  /**
+   * Check if the selected letter is in the phrase
+   * @param {string} letter - Letter to check
+   * @returns {boolean} - True if the letter is in the phrase, otherwise false
+   */
+  checkLetter(letter) {
+    return this.phrase.include(letter);
+  }
+
+  /**
+   * Reveal the matched letter on the board
+   * @param {string} letter - Letter to reveal
+   */
+  showMatchedLetter(letter) {
+    const matchingLetters = document.querySelectorAll(`.letter.${letter}`);
+
+    // Change the CSS class from 'hide' to 'show'
+    matchingLetters.forEach(letter => {
+      letter.classList.remove('hide');
+      letter.classList.add('show');
+    });
+  }
+}
